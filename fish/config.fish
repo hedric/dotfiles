5240185a9705ae9820cc6c2b3768ce042198cc2a
Richard Hedlund <richard.hedlund@gmail.com>
2023-06-29 21:29:13 +0200

    Add fish shell config file

---
+++ fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Aliases
+alias ls='ls --color=auto'
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls -CF'
+alias bc='batcat'
+
+alias glog='git log --oneline -n15'
+alias gba='git branch --all'
+alias gst='git status'
+alias gri='git rebase -i'
+alias gcv='git commit -v'
+alias gca='git commit --amend'
+
